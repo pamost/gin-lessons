77929433e0e58215f6e822ee70572435364b13c7
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-10-29 12:26:03 +0600

    User registration

--- routes.go
+++ routes.go
@@ -5,6 +5,18 @@ func initRoutes() {
 	// определение роута главной страницы
 	router.GET("/", showIndexPage)
 
+	// Group user related routes together
+	userRoutes := router.Group("/user")
+	{
+		// Handle the GET requests at /user/register
+		// Show the registration page
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.GET("/register", showRegistrationPage)
+		// Handle POST requests at /user/register
+		// Ensure that the user is not logged in by using the middleware
+		userRoutes.POST("/register", register)
+	}
+
 	// Обработчик GET-запросов на /article/view/некоторый_article_id
 	router.GET("/article/view/:article_id", getArticle)
 }
