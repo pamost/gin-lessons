773c12f5c9f3a5420792fa45da5afd7434509614
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-10-29 16:15:05 +0600

    Create article

--- routes.go
+++ routes.go
@@ -28,6 +28,18 @@ func initRoutes() {
 		userRoutes.POST("/register", register)
 	}
 
-	// Обработчик GET-запросов на /article/view/некоторый_article_id
-	router.GET("/article/view/:article_id", getArticle)
+	// Group article related routes together
+	articleRoutes := router.Group("/article")
+	{
+		// Handle GET requests at /article/view/some_article_id
+		articleRoutes.GET("/view/:article_id", getArticle)
+		// Handle the GET requests at /article/create
+		// Show the article creation page
+		// Ensure that the user is logged in by using the middleware
+		articleRoutes.GET("/create", showArticleCreationPage)
+		// Handle POST requests at /article/create
+		// Ensure that the user is logged in by using the middleware
+		articleRoutes.POST("/create", createArticle)
+	}
+
 }
